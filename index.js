562268fb63a94703b4b2e9748fce91675cb1674d
Xyper <89409852+Xyper0@users.noreply.github.com>
2021-09-02 11:33:27 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+
+# How To Use!
+` go to youtube and open console (ctrl + i + shift)
+then go to console section and put the script then click (Enter) 
+----------------------- Simple Ads Skipper ------------------------------------
+`
+*/
+
+setTimeout(() => {
+const buttonSkipper1 = document.querySelector('.ytp-ad-skip-button');
+const buttonSkipper2 = document.querySelector('.ytp-ad-overlay-close-button'); 
+
+  if(buttonSkipper1) {
+    buttonSkipper.click(); 
+    console.log('ads skip.')
+} else if(buttonSkipper2){
+  buttonSkipper2.click(); 
+  console.log('ads skip.');
+}
+},1500)
